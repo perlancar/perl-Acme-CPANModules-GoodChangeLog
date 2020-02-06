fb377a923b7afc26c57d30609aab3fa357a68931
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-07 05:42:44 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/GoodChangeLog.pm
@@ -0,0 +1,23 @@
+package Acme::CPANModules::GoodChangeLog;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => 'Modules with good ChangeLog in their releases',
+    description => <<'_',
+
+This is my subjective list of modules that I think have particularly nice
+ChangeLog entries.
+
+_
+    entries => [
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 append:SEE ALSO
+
+L<Acme::CPANModules::GoodInterfaces>
